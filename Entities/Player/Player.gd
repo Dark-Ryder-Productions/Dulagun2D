a0fcc3e3eb27c4232c86f50c91de706af81996a8
Liam F <liamf1764@gmail.com>
2022-02-11 21:09:15 -0600

    Fix in air movement. Add properties for accel and deaccel

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
 export var speed: int = 500
-export var inAirSpeed: int = 200
+export var inAirSpeedModifier: float = 0.02
+export var accel: float = 0.25
+export var friction: float = 0.1
 
 # Movement constants
 const JUMP_FORCE: int = -600
@@ -10,28 +12,23 @@ const WALL_SLIDE: int = 300
 
 # Movement properties
 var vel: Vector2 = Vector2()
-var priorXVel: int
+var priorXVel: float
 var isWallSliding: bool = false
 
 onready var sprite = $AnimatedSprite
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 func _physics_process(delta):
 	vel.x = 0
 	
 	# movement inputs
 	if Input.is_action_pressed("move_left"):
-		vel.x -= speed
-		if !is_on_floor():
-			vel.x -= clamp(inAirSpeed, -speed, inAirSpeed)
+		vel.x -= 1
 	if Input.is_action_pressed("move_right"):
-		vel.x += speed
-		if !is_on_floor():
-			vel.x += clamp(inAirSpeed, inAirSpeed, speed)
+		vel.x += 1
 		
 	# jump input
 	if Input.is_action_just_pressed("jump"):
@@ -52,19 +49,23 @@ func _physics_process(delta):
 		vel.y += GRAVITY * delta
 		isWallSliding = false
 		
-	# Momentum storage
 	if is_on_floor():
+		# Momentum storage
 		priorXVel = vel.x
-	elif vel.x == 0:
+		# sprite direction
+		if vel.x < 0:
+			sprite.flip_h = true
+		elif vel.x > 0:
+			sprite.flip_h = false
+	else:
+		# Provide player with limited control in the air
+		priorXVel = clamp((priorXVel + (vel.x * inAirSpeedModifier)), -1, 1)
 		vel.x = priorXVel
 
-	# sprite direction
-	if vel.x < 0:
-		sprite.flip_h = true
-	elif vel.x > 0:
-		sprite.flip_h = false
+	
 		
 	# applying the velocity
+	vel.x *= speed
 	vel = move_and_slide(vel, Vector2.UP)
 	
 	
