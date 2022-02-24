33ec3042d7190cf8f5a17bc7523024f05519253a
Liam F <liamf1764@gmail.com>
2022-02-23 19:02:41 -0600

    Use raycasts for wall jumping and add method typing

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -48,7 +48,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			vel.y = JUMP_FORCE
-		elif is_on_wall():
+		elif is_touching_wall():
 			vel.y = JUMP_FORCE
 			prior_x_vel = wall_jump_x_vel
 			
@@ -72,9 +72,10 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("unequip_weapons"):
 		switch_both_weapons(UNARMED)
+		
 			
 	# gravity
-	if is_on_wall() and !is_on_floor():
+	if is_touching_wall() and !is_on_floor():
 		if !is_wall_sliding:
 			# Cancel vertical velocity when player initially collides with a wall
 			vel.y = 0
@@ -101,11 +102,20 @@ func _physics_process(delta):
 	vel.x = lerp(vel.x, inputXVel * moveSpeed, accel)
 	vel = move_and_slide(vel, Vector2.UP)
 	
+# Region: Movement handling ----------------------------------------------------
+
+###
+# Get if the player is touching a wall
+# Note: Using raycasts instead of "is_on_wall()" as it is much more reliable
+###
+func is_touching_wall() -> bool:
+	return $"Wall-Collision/LeftWall".is_colliding() or $"Wall-Collision/RightWall".is_colliding()
+
 # Region: Sprite handling ------------------------------------------------------
 ###
 # Determine which direction the sprites should be facing
 ###
-func handle_sprite_direction():
+func handle_sprite_direction() -> void:
 	# sprite direction
 	if get_global_mouse_position().x < global_position.x:
 		flip_sprites(true)
@@ -117,7 +127,7 @@ func handle_sprite_direction():
 ###
 # Flip the player's main sprite horizontally and the weapon sprites vertically
 ###
-func flip_sprites(flip: bool):
+func flip_sprites(flip: bool) -> void:
 	sprite.flip_h = flip
 	if left_weapon != null:
 		left_weapon.get_node("AnimatedSprite").flip_v = flip
@@ -127,7 +137,7 @@ func flip_sprites(flip: bool):
 ###
 # Alter the z-index properties of the left and right arm
 ###
-func set_arm_z_index(leftIndex: int, rightIndex: int):
+func set_arm_z_index(leftIndex: int, rightIndex: int) -> void:
 	$"AnimatedSprite/Arm-Left".z_index = leftIndex
 	$"AnimatedSprite/Arm-Right".z_index = rightIndex
 
@@ -137,21 +147,21 @@ func set_arm_z_index(leftIndex: int, rightIndex: int):
 ###
 # Switch both left and right weapons
 ###
-func switch_both_weapons(weaponName: String):
+func switch_both_weapons(weaponName: String) -> void:
 	switch_weapon(weaponName, true)
 	switch_weapon(weaponName, false)
 
 ###
 # Switch a weapon for a given hand
 ###
-func switch_weapon(weaponName: String, isLeft: bool):
+func switch_weapon(weaponName: String, isLeft: bool) -> void:
 	unequip_weapon(isLeft)
 	equip_weapon(weaponName, isLeft)
 
 ###
 # Equip a weapon for a given hand. Assumes currently none equiped
 ###
-func equip_weapon(weaponName: String, isLeft: bool):
+func equip_weapon(weaponName: String, isLeft: bool) -> void:
 	if (isLeft and left_weapon != null and left_weapon.TYPE == weaponName) or (!isLeft and right_weapon != null and right_weapon.TYPE == weaponName):
 		return
 	
@@ -169,7 +179,7 @@ func equip_weapon(weaponName: String, isLeft: bool):
 ###
 # Unequip a current weapon for a given hand
 ###
-func unequip_weapon(isLeft: bool):
+func unequip_weapon(isLeft: bool) -> void:
 	if (isLeft and left_weapon == null) or (!isLeft and right_weapon == null):
 		return
 		
