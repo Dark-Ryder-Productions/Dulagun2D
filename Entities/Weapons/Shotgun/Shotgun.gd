d08fdf95576a30c43952d6510e14c985a0f86ee9
Liam F <liamf1764@gmail.com>
2022-02-26 19:21:31 -0600

    Add fire rate delay to shotgun

--- Entities/Weapons/Shotgun/Shotgun.gd
+++ Entities/Weapons/Shotgun/Shotgun.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+export var fire_rate: float = 0.7
 export var is_left: bool = false
 
 const NAME: String = "shotgun"
 const TYPE: String = "gun"
 const IS_AUTO: bool = false
+
+var is_firing: bool = false
 
 func _ready():
 	pass 
@@ -16,6 +19,10 @@ func _process(delta):
 # Fire a bullet where the gun is aiming
 ###
 func fire():
+	if is_firing:
+		return
+	
+	is_firing = true
 	# Placeholder until pellets are added
 	var bulletScene = load("res://Entities/Weapons/Bullet/Bullet.tscn")
 	$Muzzle.rotation_degrees = 0
@@ -27,3 +34,6 @@ func fire():
 		get_parent().add_child(bullet)
 		$Muzzle.rotation_degrees = degrees
 		bullet.global_transform = $Muzzle.global_transform
+	
+	yield(get_tree().create_timer(fire_rate), "timeout")
+	is_firing = false
